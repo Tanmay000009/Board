3e4a82ffea0fb9665803bf2be6ebdf3c0bbbb4a9
tanmay000009 <tanmayvyas009@gmail.com>
2023-09-03 21:20:24 +0530

    fix stats card and add random data

--- src/components/dashboard/stats/Stats.tsx
+++ src/components/dashboard/stats/Stats.tsx
@@ -1,22 +1,76 @@
 import StatsCards from "./StatsCards";
 
 const Stats = () => {
+  const revenueAmount = Math.floor(Math.random() * 10000000);
+  const totalTransactions = Math.floor(Math.random() * 1000);
+  const totalLikes = Math.floor(Math.random() * 100000);
+  const totalUsers = Math.floor(Math.random() * 1000000);
+  const revenuePercentage =
+    Math.round((Math.random() * 100 + Number.EPSILON) * 100) / 100;
+
+  const transactionPercentage =
+    Math.round((Math.random() * -100 + Number.EPSILON) * 100) / 100;
+  const likesPercentage =
+    Math.round((Math.random() * 100 + Number.EPSILON) * 100) / 100;
+  const usersPercentage =
+    Math.round((Math.random() * 100 + Number.EPSILON) * 100) / 100;
+
   return (
     <>
       <div className="my-8 hidden xl:flex flex-row gap-4">
-        <StatsCards active={true} />
-        <StatsCards active={false} />
-        <StatsCards active={false} />
-        <StatsCards active={false} />
+        <StatsCards
+          active={true}
+          type={"revenue"}
+          value={revenueAmount}
+          percantageChange={revenuePercentage}
+        />
+        <StatsCards
+          active={false}
+          type={"transactions"}
+          value={totalTransactions}
+          percantageChange={transactionPercentage}
+        />
+        <StatsCards
+          active={false}
+          type={"likes"}
+          value={totalLikes}
+          percantageChange={likesPercentage}
+        />
+        <StatsCards
+          active={false}
+          type={"users"}
+          value={totalUsers}
+          percantageChange={usersPercentage}
+        />
       </div>
       <div className="my-8 flex flex-row  xl:hidden gap-4">
         <div className="flex flex-col basis-1/2 justify-between items-center gap-4 md:gap-8">
-          <StatsCards active={true} />
-          <StatsCards active={false} />
+          <StatsCards
+            active={true}
+            type={"revenue"}
+            value={revenueAmount}
+            percantageChange={revenuePercentage}
+          />
+          <StatsCards
+            active={false}
+            type={"transactions"}
+            value={totalTransactions}
+            percantageChange={transactionPercentage}
+          />
         </div>
         <div className="flex flex-col basis-1/2  justify-between items-center gap-4 md:gap-8">
-          <StatsCards active={false} />
-          <StatsCards active={false} />
+          <StatsCards
+            active={false}
+            type={"likes"}
+            value={totalLikes}
+            percantageChange={likesPercentage}
+          />
+          <StatsCards
+            active={false}
+            type={"users"}
+            value={totalUsers}
+            percantageChange={usersPercentage}
+          />
         </div>
       </div>
     </>
--- src/components/dashboard/stats/StatsCards.tsx
+++ src/components/dashboard/stats/StatsCards.tsx
@@ -1,7 +1,19 @@
 import Image from "next/image";
 import { SHADOW_STYLE } from "../Dashboard";
 
-const StatsCards = ({ active }: { active: boolean }) => {
+const StatsCards = ({
+  active,
+  type,
+  value,
+  percantageChange,
+}: {
+  active: boolean;
+  type: "revenue" | "transactions" | "likes" | "users";
+  value: number;
+  percantageChange: number;
+}) => {
+  const typeString = type[0].toUpperCase() + type.slice(1);
+
   return (
     <div
       className={
@@ -9,26 +21,39 @@ const StatsCards = ({ active }: { active: boolean }) => {
         SHADOW_STYLE
       }
     >
-      <div className="bg-[#7ECC93] rounded-full p-1 md:p-2 h-10 w-10 grid place-items-center">
+      <div
+        className={
+          " rounded-full p-1 md:p-2 h-10 w-10 grid place-items-center" +
+          (type === "revenue"
+            ? " bg-[#7ECC93] "
+            : type === "transactions"
+            ? " bg-[#DEBF85] "
+            : type === "likes"
+            ? " bg-[#ECA4A4] "
+            : " bg-[#A9B0E5] ")
+        }
+      >
         <Image
-          src={"/icons/revenue.svg"}
-          alt={"Revenue icon"}
+          src={`/icons/${type}.svg`}
+          alt={`typeString icon`}
           width={24}
           height={24}
           className="hidden md:flex"
         />
         <Image
-          src={"/icons/revenue.svg"}
-          alt={"Revenue icon"}
+          src={`/icons/${type}.svg`}
+          alt={`typeString icon`}
           width={20}
           height={20}
           className="flex md:hidden"
         />
       </div>
       <div className="hidden xl:flex flex-col">
-        <h3 className="text-xs ">Total Revenues</h3>
+        <h3 className="text-xs ">Total {typeString}</h3>
         <div className="flex flex-row justify-between">
-          <h4 className="text-xl font-openSans font-bold">$ 2,299,222</h4>
+          <h4 className="text-xl font-openSans font-bold">
+            {type === "revenue" ? "$ " : ""} {value}
+          </h4>
           <div className="flex flex-row gap-1">
             {active ? (
               <Image
@@ -39,17 +64,25 @@ const StatsCards = ({ active }: { active: boolean }) => {
                 className="transform rotate-180 translate-y-4"
               />
             ) : null}
-            <div className="bg-[#E9F9EB] px-2 py-1 rounded-3xl text-sm font-figTree font-semibold text-[#4AD15F]">
-              + 2.5 %
+            <div
+              className={
+                " px-2 py-1 rounded-3xl text-sm font-figTree font-semibold]" +
+                (percantageChange > 0
+                  ? " bg-[#E9F9EB] text-[#4AD15F]"
+                  : " bg-[#FEEFEF] text-[#F05F5F]")
+              }
+            >
+              {percantageChange > 0 ? "+" + percantageChange : percantageChange}{" "}
+              %
             </div>
           </div>
         </div>
       </div>
       <div className="flex xl:hidden flex-col">
-        <h3 className="text-xs ">Total Revenue</h3>
+        <h3 className="text-xs ">Total {typeString}</h3>
         <div className="flex flex-row justify-between">
           <h4 className="text-base sm:text-lg md:text-xl font-openSans font-bold">
-            $ 2,299,222
+            {type === "revenue" ? "$ " : ""} {value}
           </h4>
           {active ? (
             <>
@@ -69,8 +102,15 @@ const StatsCards = ({ active }: { active: boolean }) => {
               />
             </>
           ) : null}
-          <div className="bg-[#E9F9EB] px-2 py-1 rounded-3xl text-[0.5rem] sm:text-xs md:text-sm font-figTree font-semibold text-[#4AD15F]">
-            + 2.5 %
+          <div
+            className={
+              " px-2 py-1 rounded-3xl text-[0.5rem] sm:text-xs md:text-sm font-figTree font-semibold " +
+              (percantageChange > 0
+                ? " bg-[#E9F9EB] text-[#4AD15F]"
+                : " bg-[#FEEFEF] text-[#F05F5F]")
+            }
+          >
+            {percantageChange > 0 ? "+" + percantageChange : percantageChange} %
           </div>
         </div>
       </div>
