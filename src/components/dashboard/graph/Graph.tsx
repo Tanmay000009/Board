b41d80633955fdae6d4413de3c3645772ad3dbd2
tanmay000009 <tanmayvyas009@gmail.com>
2023-09-08 12:26:16 +0530

    remove api

--- src/components/dashboard/graph/Graph.tsx
+++ src/components/dashboard/graph/Graph.tsx
@@ -46,51 +46,7 @@ const dummy_data = [
 
 const Graph = () => {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<any>([]);
   const [dummyData, setDummyData] = useState<boolean>(false);
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    setLoading(true);
-    const data_IBM = await fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=60min&month=2009-02&outputsize=full&apikey=${process.env.NEXT_PUBLIC_ALPHA_KEY}`
-    ).then((res) => res.json());
-
-    if (data_IBM.Information || data_IBM.Note) {
-      setData(dummy_data);
-      setLoading(false);
-      setDummyData(true);
-      return;
-    }
-
-    const IBM_data = data_IBM["Time Series (60min)"];
-
-    const data_MSFT = await fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=60min&month=2009-02&outputsize=full&apikey=${process.env.NEXT_PUBLIC_ALPHA_KEY}`
-    ).then((res) => res.json());
-
-    const MSFT_data = data_MSFT["Time Series (60min)"];
-
-    const final_data = [];
-
-    let j = 15;
-    if (IBM_data.length > 0 && MSFT_data.length > 0)
-      for (let i = 0; i < 5; i++) {
-        final_data.push({
-          name: j + ":00:00",
-          IBM: IBM_data[i]["1. open"],
-          MSFT: MSFT_data[i]["1. open"],
-        });
-        j++;
-      }
-
-    setData(final_data);
-
-    setLoading(false);
-  };
 
   return (
     <div
@@ -112,7 +68,7 @@ const Graph = () => {
         </div>
       ) : (
         <ResponsiveContainer width="100%" height="100%">
-          <BarChart data={data} maxBarSize={40}>
+          <BarChart data={dummy_data} maxBarSize={40}>
             <CartesianGrid vertical={false} stroke="#EAEAEA" />
             <XAxis
               dataKey="name"
@@ -126,11 +82,6 @@ const Graph = () => {
             <Bar dataKey="MSFT" fill="#82ca9d" radius={[5, 5, 5, 5]} />
           </BarChart>
         </ResponsiveContainer>
-      )}
-      {dummyData && (
-        <p className="font-mont font-normal text-sm text-[#858585]">
-          {"API limit reached, hence using dummy data :)"}
-        </p>
       )}
     </div>
   );
